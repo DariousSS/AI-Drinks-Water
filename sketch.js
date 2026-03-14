383a9df6d137755a5201a903c8e4c604a0c26bf7
DariousSS <Zoe796756765@gmail.com>
2026-03-14 15:25:51 +0000

    3

--- sketch.js
+++ sketch.js
@@ -17,7 +17,11 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+<<<<<<< Updated upstream
   pixelDensity(1 / 5);
+=======
+  pixelDensity(1);
+>>>>>>> Stashed changes
   noStroke();
   textAlign(CENTER, CENTER);
   textFont(font);
@@ -33,13 +37,24 @@ function generateLetters() {
   vy = [];
   sizes = [];
 
+<<<<<<< Updated upstream
   spacing = random(20, 300);
 
+=======
+  // 大范围随机密度：从稀疏(10几个)到密集(像素感)
+  spacing = random(20, 300);
+
+  // 性能保护：单屏最多约2500个，防止太密卡顿
+>>>>>>> Stashed changes
   let minSpacing = sqrt((width * height) / 2500);
   if (spacing < minSpacing) spacing = minSpacing;
 
   alpha = random(60, 220);
 
+<<<<<<< Updated upstream
+=======
+  // 文字大小跟密度绑定：越密越小，像素感；越疏越大
+>>>>>>> Stashed changes
   let baseSize = spacing * random(0.5, 0.95);
 
   for (let px = spacing / 2; px < width; px += spacing) {
@@ -66,12 +81,17 @@ function draw() {
   for (let i = 0; i < x.length; i++) {
     let d = dist(x[i], y[i], mouseX, mouseY);
 
+<<<<<<< Updated upstream
+=======
+    // 鼠标靠近时排斥
+>>>>>>> Stashed changes
     if (d < 120 && d > 0) {
       let force = (1 - d / 120) * 18;
       vx[i] += ((x[i] - mouseX) / d) * force;
       vy[i] += ((y[i] - mouseY) / d) * force;
     }
 
+<<<<<<< Updated upstream
     vx[i] += (homeX[i] - x[i]) * 0.06;
     vy[i] += (homeY[i] - y[i]) * 0.06;
 
@@ -81,6 +101,21 @@ function draw() {
     x[i] += vx[i];
     y[i] += vy[i];
 
+=======
+    // 弹回原位
+    vx[i] += (homeX[i] - x[i]) * 0.06;
+    vy[i] += (homeY[i] - y[i]) * 0.06;
+
+    // 阻尼
+    vx[i] *= 0.75;
+    vy[i] *= 0.75;
+
+    // 更新位置
+    x[i] += vx[i];
+    y[i] += vy[i];
+
+    // 离家越远越透明
+>>>>>>> Stashed changes
     let displacement = dist(x[i], y[i], homeX[i], homeY[i]);
     let a = map(displacement, 0, 150, alpha, alpha * 0.3);
 
